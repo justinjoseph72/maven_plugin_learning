41eab33fcf62f132090ed7677928d58bef8ac615
justinjoseph72 <justin.joseph72@outlook.com>
2019-02-10 20:29:12 +0000

    updating the pom to have libraary and main class in the manifest

---
+++ src/test/java/com/justin/app/integration_test/SampleIT.java
@@ -0,0 +1,13 @@
+package com.justin.app.integration_test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SampleIT {
+
+
+    @Test
+    public void shouldRunItTest(){
+        Assert.assertTrue(true);
+    }
+}
